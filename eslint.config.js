e3fb6587331cf11cc081ba0f4ce613507515cd62
Le Vivilet <levivilet@pm.me>
2026-08-01 17:40:38 +0000

    feature: update eslint dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
+import { defineConfig } from 'eslint/config'
 import * as config from '@lvce-editor/eslint-config'
-import * as actions from '@lvce-editor/eslint-plugin-github-actions'
 
-export default [
+export default defineConfig([
   ...config.default,
-  ...actions.default,
+  ...config.recommendedActions,
   {
     rules: {
       '@typescript-eslint/ban-ts-comment': 'off',
@@ -37,4 +37,4 @@ export default [
       'e2e/no-imports': 'off',
     },
   },
-]
+])
